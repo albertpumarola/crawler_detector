400f0fa8559d24095ab69fc989a92a8b267098f6
asantamaria <asantamaria@iri.upc.edu>
2018-09-10 13:04:58 +0200

    published pose in 3D canonic vector

--- src/crawler_detector_node.py
+++ src/crawler_detector_node.py
@@ -7,6 +7,7 @@ import roslib
 import rospy
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
+from sensor_msgs.msg import CameraInfo
 from geometry_msgs.msg import PoseStamped
 from CrawlerDetector.api import CrawlerDetector
 
@@ -14,6 +15,7 @@ class CrawlerDetectorNode:
     def __init__(self):
         self.bridge = CvBridge()
         self._cam_sub = rospy.Subscriber("/robot/image_raw/", Image, self.callback, queue_size=1)
+        self._cam_info_sub = rospy.Subscriber("/robot/camera_info", CameraInfo, self.cam_info_callback, queue_size=1)
         self._pose_pub = rospy.Publisher("/crawler/pose", PoseStamped, queue_size=1)
         self._do_display = rospy.get_param("/crawler_detector/do_display_detection") == True
 	self._pose_tf = rospy.get_param("/crawler_detector/pose_tf")
@@ -26,20 +28,27 @@ class CrawlerDetectorNode:
 	    self._publish_pose(uv_max)
 	    self._new_image = False
 
-    def callback(self, ros_data):
+    def callback(self, msg):
+           self._fx = msg.P[0]
+           self._cx = msg.P[2]
+           self._fy = msg.P[5]
+           self._cy = msg.P[6]
+
+    def cam_info_callback(self, ros_data):
 	try:
            self._image_np = self.bridge.imgmsg_to_cv2(ros_data, "bgr8")
 	   self._new_image = True
         except CvBridgeError as e:
             print(e)
 
+
     def _publish_pose(self, uv):
         msg = PoseStamped()
         msg.header.frame_id = self._pose_tf
         msg.header.stamp = rospy.Time.now()
-        msg.pose.position.z = 0
-        msg.pose.position.x = uv[0]
-        msg.pose.position.y = uv[1]
+        msg.pose.position.z = 1.0
+        msg.pose.position.x = (uv[0]-self._cx)/self._fx
+        msg.pose.position.y = (uv[1]-self._cy)/self._fy
         msg.pose.orientation.w = 1.0
         self._pose_pub.publish(msg)
 
